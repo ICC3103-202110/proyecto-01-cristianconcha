d8b22919c2e1132c6a3d63a4fab6cdb4d92dbae6
CristianConcha <ciconcha@miuandes.cl>
2021-04-11 20:25:56 -0400

    Duke, Assassinate,Exchange, Steal actions

--- game.py
+++ game.py
@@ -5,7 +5,8 @@ class Game:
     print("-----------------------------------")
     print("---------Wellcome to Coup----------")
     print("-----------------------------------\n")
-    NUMBER_Players = 3#int(input("Enter the number of playeres (3 or 4): "))
+    NUMBER_Players = 3  #int(input("Enter the number of playeres (3 or 4): "))
+    card = Cards()
     players = []
     turn = 0
     Number_Action = 0
@@ -47,9 +48,9 @@ class Game:
     @classmethod
     def name_players(cls):
         for i in range(1, cls.NUMBER_Players + 1):
-            card = Cards()
+            
             name = input("Give the player's %d name: " % i)
-            cls.players.append(Player(name, card.randomCards()))
+            cls.players.append(Player(name, cls.card.randomCards()))
 
     @classmethod
     def print_Coins(cls):
@@ -85,7 +86,7 @@ class Game:
 
 
     @classmethod
-    def Action(cls):
+    def Action(cls): #corroborate inputs numbers
         if cls.Number_Action == 1: #Income
             cls.players[cls.turn].add_one_coin()
         
@@ -102,14 +103,57 @@ class Game:
                 
                 select = int(input(("Choose the player to lose Influence:")))
                 cls.players[select].delete_card()
-                cls.players[cls.turn].pay_7_coins()
+                cls.players[cls.turn].pay_seven_coins()
                 
             else:
-                print("You don`t have the money")
+                print("You don`t have 7 coins")
                 #Hacer que se repita el ciclo
         
+        elif cls.Number_Action == 4:  # Duke (tax)
+            cls.players[cls.turn].add_three_coin()
+        
+        elif cls.Number_Action == 5:  # Assassinate (assassinate)
+            if cls.players[cls.turn].coin >= 3:
+                for i in range(cls.NUMBER_Players):
+                    if i == cls.turn:
+                        continue
+                    else:
+                        print(i, cls.players[i].player)
+                
+                select = int(input(("Choose the player to lose Influence:")))
+                cls.players[select].delete_card()
+                cls.players[cls.turn].pay_three_coins()
+                
+            else:
+                print("You don`t have 3 coins")
+                #Hacer que se repita el ciclo
+
+        elif cls.Number_Action == 6:  # Ambassador (Exhange)
+            card = cls.card.randomCards()
+            cls.players[cls.turn].add_two_cards(card)
+            cls.players[cls.turn].delete_two_cards()
+            
+        elif cls.Number_Action == 7:  # Ambassador (Steal)
+            for i in range(cls.NUMBER_Players):
+                if i == cls.turn:
+                    continue
+                else:
+                    print(i, cls.players[i].player, "have",cls.players[i].coin, "coins")
+
+            select = int(input(("Choose the player to lose 2 coins:")))
+            cls.players[select].delete_two_coins()
+            cls.players[cls.turn].add_two_coin()
         
 
+                
+
+
+
+
+
+
+
+        
 
 
             
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from cards import Cards
 class Player:
-    def __init__(self, player, cards= 0, coin = 7):
+    def __init__(self, player, cards, coin = 7): #cambiar coins
         self.__player = player
         self.cards = cards
         self.coin = coin
@@ -9,11 +9,10 @@ class Player:
     def player(self):
         return self.__player
     
-
-
     def printCard(self):
         for i in range(len(self.cards)):
-            print(self.cards[i])
+            print(self.cards[i], end=" ")
+        print("")
 
     def printCoins(self):
         print(self.player + ":", end = " ")
@@ -25,11 +24,40 @@ class Player:
     def add_two_coin(self):
         self.coin += 2
     
-    def pay_7_coins(self):
+    def add_three_coin(self):
+        self.coin += 3
+    
+    def delete_two_coins(self):
+        self.coin -= 2
+        
+    def pay_three_coins(self):
+        self.coin -= 3
+
+    def pay_seven_coins(self):
         self.coin -= 7
 
     def delete_card(self):
         self.cards.pop()
+    
+    def add_two_cards(self, card):
+        self.cards += card
+    
+    def delete_two_cards(self):
+        for a in range(2):
+            for i in range(len(self.cards)):
+                print(i, self.cards[i])
+            
+            delete = int(input("Choose the number card to delete: "))
+            self.cards.pop(delete)
+            print("")
+
+
+
+
+
+
+
+
     
    
     
