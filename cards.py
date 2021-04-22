140da829b6ac67a1c2699474a545bae70fe67b54
CristianConcha <ciconcha@miuandes.cl>
2021-04-21 20:29:28 -0400

    private values in cards

--- cards.py
+++ cards.py
@@ -4,10 +4,26 @@ from console import Console
 
 class Cards:
     def __init__(self, cards=0, cards_lose=0):
-        self.cards = ["Duke", "Assassin", "Ambassador", "Captain", "Contessa",
+        self.__cards = ["Duke", "Assassin", "Ambassador", "Captain", "Contessa",
                         "Duke", "Assassin", "Ambassador", "Captain", "Contessa",
                         "Duke", "Assassin", "Ambassador", "Captain", "Contessa"]
-        self.cards_lose = []
+        self.__cards_lose = []
+    
+    @property
+    def cards(self):
+        return self.__cards
+
+    @cards.setter
+    def cards(self, value):
+        self.__cards = value
+
+    @property
+    def cards_lose(self):
+        return self.__cards_lose
+
+    @cards_lose.setter
+    def cards_lose(self, value):
+        self.__cards_lose = value
 
     #random cards
     def two_random_cards(self):
